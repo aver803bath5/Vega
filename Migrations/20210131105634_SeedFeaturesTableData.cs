414293bb3339270206fc687403e7f8d9372e0bad
aver803bath5 <aver803bath261@gmail.com>
2021-01-31 19:09:20 +0800

    feat(migrations): seed data for Features table

---
+++ Migrations/20210131105634_SeedFeaturesTableData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedFeaturesTableData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('feature1')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('feature2')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('feature3')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE NAME IN ('feature1', 'feature2', 'feature3')");
+        }
+    }
+}
