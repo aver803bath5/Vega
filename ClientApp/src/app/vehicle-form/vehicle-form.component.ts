2bef989ca8c8808d81fccab68a6d9cefeb91b3d2
aver803bath5 <aver803bath261@gmail.com>
2021-02-04 15:54:12 +0800

    feat(vehicla-form): add validators
    * Add validators for contact name, phone, email inputs.
    * Do some refactors.

--- ClientApp/src/app/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {FormArray, FormBuilder, FormControl, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, ValidatorFn, Validators} from "@angular/forms";
 
-import { VehicleFormService } from "../vehicle-form.service";
+import {VehicleFormService} from "../vehicle-form.service";
 import {IMake} from "../../core/models/IMake";
 import {IModel} from "../../core/models/IModel";
 import {IFeature} from "../../core/models/IFeature";
@@ -18,9 +18,9 @@ export class VehicleFormComponent implements OnInit {
     model: [0, Validators.min(1)],
     features: new FormArray([]),
     isRegistered: false,
-    contactName: "",
-    contactPhone: "",
-    contactEmail: "",
+    contactName: ["", [Validators.maxLength(255), Validators.required]],
+    contactPhone: ["", [Validators.maxLength(255), Validators.required]],
+    contactEmail: ["", [Validators.email, Validators.maxLength(255), Validators.required]],
   });
   makes = new Array<IMake>();
   models = new Array<IModel>();
@@ -32,39 +32,35 @@ export class VehicleFormComponent implements OnInit {
 
   constructor(
     private vehicleFormService: VehicleFormService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
-    this.vehicleFormService.getMakes().subscribe(result  => {
-      this.makes.push(...result);
+    this.vehicleFormService.getMakes().subscribe(makes => {
+      this.makes = makes;
     });
 
-    this.vehicleFormService.getFeatures().subscribe(result => {
-      this.features.push(...result);
-      result.forEach(() => this.featuresFormArray.push(new FormControl(false)));
+    this.vehicleFormService.getFeatures().subscribe(features => {
+      this.features = features;
+      features.forEach(() => this.featuresFormArray.push(new FormControl(false)));
     });
   }
 
-  onMakeSelect(event) {
-    const makeId = event.target.value;
+  onMakeSelect() {
+    const makeId = this.form.controls.make.value;
     this.form.patchValue({
       model: 0
     });
 
-    for (let i = 0; i < this.makes.length; i++)
-    {
-      if (this.makes[i].id.toString() === makeId)
-      {
-        this.models.push(...this.makes[i].models);
-        return;
-      }
-    }
+    this.models = this.makes.find(m => m.id == makeId).models;
   }
 
   onSubmit() {
     const selectedFeatureIds = this.form.value.features
       .map((checked, i) => checked ? this.features[i].id : checked)
       .filter(v => v !== false);
+
+
     console.log(this.form.value);
   }
 }
