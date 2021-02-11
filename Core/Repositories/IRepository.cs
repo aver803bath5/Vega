81b38f2d0d5ea08879c5c2b14d76fbfea31ea940
aver803bath5 <aver803bath261@gmail.com>
2021-02-11 14:20:48 +0800

    feat(IRepository): add async methods

--- Core/Repositories/IRepository.cs
+++ Core/Repositories/IRepository.cs
@@ -1,19 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Vega.Core.Repositories
 {
     public interface IRepository<TEntity> where TEntity : class
     {
         TEntity Get(int id);
+        Task<TEntity> GetAsync(int id);
+        
         IEnumerable<TEntity> GetAll();
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        
         IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate);
 
         TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
 
         void Add(TEntity entity);
         void AddRange(IEnumerable<TEntity> entities);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
 
         void Remove(TEntity entity);
         void RemoveRange(IEnumerable<TEntity> entities);
