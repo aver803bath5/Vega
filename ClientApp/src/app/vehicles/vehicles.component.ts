78b317df9ab76ef1071fbf9d3fb6134a9b58907c
aver803bath5 <aver803bath261@gmail.com>
2021-03-01 11:08:49 +0800

    feat(vehicles-table-component): dynamically generate column heads
    * Dynamically generate table head row columns to reduce works to hand write similar html for table head columns.
    * Use one method to sort Vehicles instead of three methods because there are many common this in `onClickMake`, `onClickModel` and `onClickContactName` like check if currently sorted by the same property and change to sort order and do the work to sorting vehicles with specific key.

--- ClientApp/src/app/vehicles/vehicles.component.ts
+++ ClientApp/src/app/vehicles/vehicles.component.ts
@@ -5,14 +5,19 @@ import { faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
 import { VehicleService } from "../vehicle.service";
 import { IVehicle } from "../shared/models/IVehicle";
 import { IMake } from "../shared/models/IMake";
-import { forkJoin, Observable } from "rxjs";
 import { ActivatedRoute, Router } from "@angular/router";
 import { IPagination } from "./IPagination";
 
-enum Order {
-  Non,
-  Descending,
-  Ascending
+interface IQueryParameters {
+  pageNumber?: number;
+  makeId?: number;
+  orderBy?: string;
+}
+
+interface IColumn {
+  title: string;
+  key?: string;
+  isSortable?: boolean;
 }
 
 @Component({
@@ -26,20 +31,20 @@ export class VehiclesComponent implements OnInit {
   tableVehicles: Array<IVehicle> = [];
   makes: Array<IMake> = [];
   selectedMake = 0;
-  makeOrder = Order.Non;
-  modelOrder = Order.Non;
-  contactNameOrder = Order.Non;
   faSort = null;
   pagination: IPagination = null;
-  queryParameters = {
+  queryParameters: IQueryParameters = {
     pageNumber: 1,
     makeId: 0,
     orderBy: "",
   }
-
-  get sortLength() {
-    return Object.keys(Order).filter(x => isNaN(Number(x))).length;
-  }
+  columns: IColumn[] = [
+    { title: 'Id' },
+    { title: 'Make', key: 'model.make.name', isSortable: true },
+    { title: 'Model', key: 'model.name', isSortable: true },
+    { title: 'Contact Name', key: 'contact.name', isSortable: true },
+    { title: '' },
+  ]
 
   constructor(
     private vehicleService: VehicleService,
@@ -49,17 +54,22 @@ export class VehiclesComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.vehicleService.getMakes().subscribe(makes => this.makes = makes);
     this.route.queryParams.subscribe(params => {
-      this.queryParameters.pageNumber = +params.pageNumber;
-      this.queryVehicles();
+      if (params.pageNumber)
+        this.queryParameters.pageNumber = +params.pageNumber;
+      this.populateVehicles();
     });
   }
 
 
   onMakeChange() {
-    this.queryParameters.pageNumber = 1;
-    this.queryParameters.makeId = this.selectedMake;
-    this.router.navigate(['/vehicles'], {queryParams: this.queryParameters});
+    this.queryParameters = {
+      pageNumber: 1,
+      makeId: this.selectedMake,
+      orderBy: ''
+    };
+    this.router.navigate(['/vehicles'], { queryParams: this.queryParameters }).then();
   }
 
   // Client-Side filter
@@ -70,77 +80,27 @@ export class VehiclesComponent implements OnInit {
   //     this.filteredVehicles = _.filter(this.vehicles, v => v.make.id === this.selectMake);
   //   }
   // }
-  onClickMake() {
-    this.makeOrder === Order.Non && this.resetOrders();
-    this.makeOrder = this.changeOrder(this.makeOrder);
-    this.sortVehicles("model.make.name", this.makeOrder);
+
+  setSortIcon() {
+    if (this.queryParameters.orderBy.includes('desc'))
+      return faSortDown;
+    else
+      return faSortUp;
   }
 
-  onClickModel() {
-    this.modelOrder === Order.Non && this.resetOrders();
-    this.modelOrder = this.changeOrder(this.modelOrder);
-    this.sortVehicles("model.name", this.modelOrder);
-  }
-
-  onClickContactName() {
-    this.contactNameOrder === Order.Non && this.resetOrders();
-    this.contactNameOrder = this.changeOrder(this.contactNameOrder);
-    this.sortVehicles("contact.name", this.contactNameOrder);
-  }
-
-  setSortIcon(order = Order.Non) {
-    switch (order) {
-      case Order.Descending:
-        return faSortDown;
-      case Order.Ascending:
-        return faSortUp;
-      default:
-        return null;
-    }
-  }
-
-  private queryVehicles() {
-    const source: Array<Observable<any>> = [this.vehicleService.getVehicles(this.queryParameters), this.vehicleService.getMakes()];
-    forkJoin(source).subscribe(data => {
-      this.setTableData(data[0]);
-      this.makes = [...data[1]];
+  private populateVehicles() {
+    this.vehicleService.getVehicles(this.queryParameters).subscribe(res => {
+      this.tableVehicles = [...res.body as Array<IVehicle>];
+      this.pagination = JSON.parse(res.headers.get('x-pagination'));
     });
   }
 
-  private resetOrders() {
-    this.makeOrder = Order.Non;
-    this.modelOrder = Order.Non;
-    this.contactNameOrder = Order.Non;
-  }
+  sortVehicles(orderBy) {
+    if (this.queryParameters.orderBy === orderBy)
+      this.queryParameters.orderBy = `${ orderBy } desc`;
+    else
+      this.queryParameters.orderBy = orderBy;
 
-  private sortVehicles(orderBy = "", order = Order.Non) {
-    switch (order) {
-      case Order.Descending:
-        this.queryParameters.orderBy = `${ orderBy } desc`
-        this.vehicleService.getVehicles(this.queryParameters).subscribe(res => {
-          this.tableVehicles = [...res.body as Array<IVehicle>];
-        });
-        break;
-      case Order.Ascending:
-        this.queryParameters.orderBy = `${ orderBy }`
-        this.vehicleService.getVehicles(this.queryParameters).subscribe(res => {
-          this.tableVehicles = [...res.body as Array<IVehicle>];
-        });
-        break;
-      default:
-        this.queryParameters.orderBy = "";
-        this.vehicleService.getVehicles(this.queryParameters).subscribe(res => {
-          this.tableVehicles = [...res.body as Array<IVehicle>];
-        });
-    }
-  }
-
-  private changeOrder(sort: Order) {
-    return (sort + 1) % this.sortLength;
-  }
-
-  private setTableData(res) {
-    this.tableVehicles = [...res.body as Array<IVehicle>];
-    this.pagination = JSON.parse(res.headers.get('x-pagination'));
+    this.populateVehicles();
   }
 }
