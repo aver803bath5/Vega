7129fa1b0f9b9cf4ba6391a6bb971f5b79c9e1a0
aver803bath5 <aver803bath261@gmail.com>
2021-03-07 11:39:37 +0800

    feat(domain-models): add photos table
    * Make Vehicles and Photos relationship to be one-or-none to many so I add a foreign key which is a index of vehicleId in Photos table.

---
+++ Migrations/20210307033335_PhotosTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class PhotosTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Photos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Photos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Photos");
+        }
+    }
+}
