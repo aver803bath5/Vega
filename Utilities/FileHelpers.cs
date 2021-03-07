5aeae8551e98aea70c0946f6966548fdb3e24d5d
aver803bath5 <aver803bath261@gmail.com>
2021-03-07 19:28:23 +0800

    feat(Utilities): add FileHelpers
    * Use FileHelpers class to proceed uploaded file.
    * It can check if file size, file types are validate.

---
+++ Utilities/FileHelpers.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Vega.Utilities
+{
+    public static class FileHelpers
+    {
+        private static readonly Dictionary<string, List<byte[]>> _fileSignature = new Dictionary<string, List<byte[]>>
+        {
+            {".gif", new List<byte[]> {new byte[] {0x47, 0x49, 0x46, 0x38}}},
+            {".png", new List<byte[]> {new byte[] {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A}}},
+            {
+                ".jpeg", new List<byte[]>
+                {
+                    new byte[] {0xFF, 0xD8, 0xFF, 0xE0},
+                    new byte[] {0xFF, 0xD8, 0xFF, 0xE2},
+                    new byte[] {0xFF, 0xD8, 0xFF, 0xE3},
+                }
+            },
+            {
+                ".jpg", new List<byte[]>
+                {
+                    new byte[] {0xFF, 0xD8, 0xFF, 0xE0},
+                    new byte[] {0xFF, 0xD8, 0xFF, 0xE1},
+                    new byte[] {0xFF, 0xD8, 0xFF, 0xE8},
+                }
+            },
+        };
+
+        public static async Task<byte[]> ProcessFormFile<T>(IFormFile formFile, ModelStateDictionary modelState,
+            string[] permittedExtensions, long sizeLimit)
+        {
+            var fieldDisplayName = string.Empty;
+
+            // Use reflection to obtain the display name for the model
+            // property associated with this IFormFile. If a display
+            // name isn't found, error messages simply won't show
+            // a display name.
+            MemberInfo property = typeof(T).GetProperty(
+                formFile.Name.Substring(formFile.Name.IndexOf(".",
+                    StringComparison.Ordinal) + 1));
+
+            if (property != null)
+            {
+                if (property.GetCustomAttribute(typeof(DisplayAttribute)) is
+                    DisplayAttribute displayAttribute)
+                {
+                    fieldDisplayName = $"{displayAttribute.Name} ";
+                }
+            }
+
+            var trustedFileNameForDisplay = WebUtility.HtmlEncode(formFile.FileName);
+
+            // Check the file length. This check doesn't catch files that only have 
+            // a BOM as their content.
+            if (formFile.Length == 0)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName}({trustedFileNameForDisplay} is Empty");
+
+                return Array.Empty<byte>();
+            }
+
+            if (formFile.Length > sizeLimit)
+            {
+                var megabytesSizeLimit = sizeLimit / 1048576;
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName}({trustedFileNameForDisplay}) exceeds" + $"{megabytesSizeLimit:N1} MB.");
+                return Array.Empty<byte>();
+            }
+
+            try
+            {
+                await using var memoryStream = new MemoryStream();
+                await formFile.CopyToAsync(memoryStream);
+
+                // Check the content length in case the file's only
+                // content was a BOM and the content is actually
+                // empty after removing the BOM. 
+                if (memoryStream.Length == 0)
+                {
+                    modelState.AddModelError(formFile.Name,
+                        $"{fieldDisplayName}({trustedFileNameForDisplay}) is Empty");
+                }
+
+                if (!IsValidFileExtensionAndSignature(formFile.FileName, memoryStream, permittedExtensions))
+                {
+                    modelState.AddModelError("File",
+                        "The file type isn't permitted or the file's " +
+                        "signature doesn't match the file's extension.");
+                }
+                else
+                {
+                    return memoryStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                modelState.AddModelError(formFile.Name,
+                    $"{fieldDisplayName}({trustedFileNameForDisplay}) upload failed. " +
+                    $"Please contact the Help Desk for support. Error: {ex.Message}");
+                // Log the exception
+            }
+
+            return Array.Empty<byte>();
+        }
+
+        private static bool IsValidFileExtensionAndSignature(string fileName, Stream data, string[] permittedExtensions)
+        {
+            if (string.IsNullOrWhiteSpace(fileName) || data == null || data.Length == 0)
+            {
+                return false;
+            }
+
+            var ext = Path.GetExtension(fileName).ToLowerInvariant();
+
+            if (string.IsNullOrWhiteSpace(ext) || !permittedExtensions.Contains(ext))
+            {
+                return false;
+            }
+
+            data.Position = 0;
+
+            // File signature check
+            // --------------------
+            // With the file signatures provided in the _fileSignature
+            // dictionary, the following code tests the input content's
+            // file signature. 
+            using var reader = new BinaryReader(data);
+            var signatures = _fileSignature[ext];
+            var headerBytes = reader.ReadBytes(signatures.Max(m => m.Length));
+
+            return signatures.Any(signature =>
+                headerBytes.Take(signature.Length).SequenceEqual(signature));
+        }
+    }
+}
