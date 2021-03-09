89306bd6dfae25ef90279d2b194332df7d0a1d8a
aver803bath5 <aver803bath261@gmail.com>
2021-03-09 20:13:07 +0800

    feat(domain-model): make add Vehicle navigation property to Photos
    * Add the Vehicle navigation property to Photos so the VehicleId in the Photos table become non-nullable.

---
+++ Migrations/20210309114137_MakeVehicleIdColumnInPhotosTableNotNullable.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class MakeVehicleIdColumnInPhotosTableNotNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Vehicles_VehicleId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "Photos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Vehicles_VehicleId",
+                table: "Photos",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_Vehicles_VehicleId",
+                table: "Photos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleId",
+                table: "Photos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_Vehicles_VehicleId",
+                table: "Photos",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
