15afa4748210927958c01f95269bef887fa28b48
aver803bath5 <aver803bath261@gmail.com>
2021-03-10 18:25:06 +0800

    feat(domain): add RequestPath column to Photos table
    * Add request path for photo data so that front-end can use the path to get the photo file.
    * I also make the FilePath column non-nullable.

--- Controllers/Resources/PhotoResource.cs
+++ Controllers/Resources/PhotoResource.cs
@@ -3,6 +3,6 @@ namespace Vega.Controllers.Resources
     public class PhotoResource
     {
         public int Id { get; set; }
-        public string FilePath { get; set; }
+        public string RequestPath { get; set; }
     }
 }
--- Core/Domain/Photo.cs
+++ Core/Domain/Photo.cs
@@ -5,6 +5,7 @@ namespace Vega.Core.Domain
         public int Id { get; set; }
 
         public string FilePath { get; set; }
+        public string RequestPath { get; set; }
 
         public Vehicle Vehicle { get; set; }
         public int VehicleId { get; set; }
