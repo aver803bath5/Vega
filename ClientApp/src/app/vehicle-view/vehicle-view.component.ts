71157a3c88ee685046ebea0cbdc103c55cea4c08
aver803bath5 <aver803bath261@gmail.com>
2021-03-12 16:16:10 +0800

    feat(vehicle-view): display error message in a toast after uploading files
    * If user cancels the file selecting window which will set the value of the file upload input to empty string. So if there is a value existed in the file input, it will change the value of the input to empty string which will also trigger the change event of the file input so I add a if statement to check if the input value is empty to prevent triggering file upload request when there is no value in the file upload input.

--- ClientApp/src/app/vehicle-view/vehicle-view.component.ts
+++ ClientApp/src/app/vehicle-view/vehicle-view.component.ts
@@ -36,14 +36,21 @@ export class VehicleViewComponent implements OnInit {
 
   onFileUpload(event) {
     const files: FileList = event.target.files;
+    if (files.length === 0) return;
+
     const formData = new FormData();
     for (let i = 0; i < files.length; i++) {
       if (files[i])
         formData.append('files', files[i]);
     }
-    this.vehicleService.uploadPhotos(this.vehicleId, formData).subscribe(x => {
+    this.vehicleService.uploadPhotos(this.vehicleId, formData).subscribe(() => {
       this.toastr.success('Photo has been uploaded', 'Success');
+      // reset file input value
+      event.target.value = '';
       this.getPhotos();
+    }, error => {
+      if (error.status == 400)
+        error.error.files.forEach(errorMessage => this.toastr.error(errorMessage));
     });
   }
 
