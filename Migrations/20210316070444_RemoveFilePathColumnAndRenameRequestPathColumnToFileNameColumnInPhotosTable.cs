cf86fbb5fed39fc5276de5931e4d0ab3ac2596dc
aver803bath5 <aver803bath261@gmail.com>
2021-03-16 15:42:46 +0800

    feat(domain-model): add FileName property to Photo model
    * Remove FilePath property and rename RequestPath to FilePath in Photos table because I found that it was unnecessary to store the request url in the table. Because the I can config the the URL to serve the files no matter where I store them and I also store the physical path in the appsettings file so it was also unnecessary to store the physical path in the table I can store it as the Controller property since it is a fixed value.

---
+++ Migrations/20210316070444_RemoveFilePathColumnAndRenameRequestPathColumnToFileNameColumnInPhotosTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class RemoveFilePathColumnAndRenameRequestPathColumnToFileNameColumnInPhotosTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FilePath",
+                table: "Photos");
+
+            migrationBuilder.RenameColumn(
+                name: "RequestPath",
+                table: "Photos",
+                newName: "FileName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FileName",
+                table: "Photos",
+                newName: "RequestPath");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FilePath",
+                table: "Photos",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
